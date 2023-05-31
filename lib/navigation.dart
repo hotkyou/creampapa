bd0d2217271eb489aea469722c7c5147cd89dd50
LAPTOP-TF4E40PG\2210358 <2210358@ecc.ac.jp>
2023-06-01 02:01:03 +0900

    add_navigation

---
+++ lib/navigation.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'package:creampapa/screens/home.dart';
+import 'package:creampapa/screens/screen_alpha.dart';
+import 'package:creampapa/screens/screen_beta.dart';
+import 'package:creampapa/screens/setting.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  static const _screens = [
+    HomeScreen(),
+    ScreenAlpha(),
+    ScreenBeta(),
+    SettingScreen(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: 'お知らせ'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: '設定'),
+          ],
+          type: BottomNavigationBarType.fixed,
+        ));
+  }
+}
