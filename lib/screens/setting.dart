4e1cf889b80faa7e5ececbbb71f11753d77a9232
hotkyou <hottarakasi.stu@gmail.com>
2023-06-05 14:23:28 +0900

    add-ChangeText

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -7,7 +7,7 @@ class SettingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('ホーム'),
+          title: const Text('設定'),
         ),
         body: const Center(
           child: Text(
